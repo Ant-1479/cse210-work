02d054e50f3fd1b89071dab054cc5e56bb8e1585
steph-1479 <antwiwaahstephanie71@gmail.com>
2026-01-10 22:50:55 -1200

    Program.cs week01\Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,39 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    {  Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // For Parts 1 and 2, where the user specified the number...
+        // Console.Write("What is the favourite number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        
+        // For Part 3, where we use a random number
+        Random randomGenerator = new Random();
+        int favouriteNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        // We could also use a do-while loop here...
+        while (guess != favouriteNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (favouriteNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (favouriteNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
+    }
