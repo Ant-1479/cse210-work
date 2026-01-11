ddb6ba6e021cb0cdaf9ccad03a2448939b77f254
steph-1479 <antwiwaahstephanie71@gmail.com>
2026-01-10 20:35:18 -1200

    Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!Sandbox Project is here.");
+        Console.WriteLine("Hello World!This is the sandbox project.");
     }
 }
